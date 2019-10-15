53dfc148c988b9d8882c4868a2d3f7ca8a38b7ad
Jai Sachdeva <jai@flick2know.com>
2019-10-15 13:00:53 +0530

    Added basic CI/CD workflow

--- test/news_respository_test.dart
+++ test/news_respository_test.dart
@@ -1,7 +1,3 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:news_reader/core/data/network/i_news_api.dart';
-import 'package:news_reader/core/data/repositories/news_repository.dart';
-
 void main() {
 
 }
